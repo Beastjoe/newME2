9a5f1582dc786a4d3910d0c91511a6802bad2b98
Beastjoe <joe_joe@sjtu.edu.cn>
2017-11-18 20:01:06 -0500

    asd

--- minuku2-extended/src/main/java/edu/umich/si/inteco/minuku/config/Constants.java
+++ minuku2-extended/src/main/java/edu/umich/si/inteco/minuku/config/Constants.java
@@ -245,5 +245,6 @@ public class Constants {
 
     public static final String DIABETES_LOG_NOTIFICATION_SOURCE = "DIABETES_LOG";
 
+    public static long LOCATION_SAMPLE_FREQUENCY = 15; //15 is 1 minute
 
 }
